be9d724bf9461006aa86e069a4f01e09998de3bd
Ariel2774 <67646945+Ariel2774@users.noreply.github.com>
2021-08-21 23:18:05 +0300

    Add files via upload

---
+++ config/bot.js
@@ -0,0 +1,6 @@
+require('dotenv').config();
+
+module.exports = {
+    token: "ODczNjUxMTM3Nzg4Njc0MTI5.YQ7hBg.bB1Wu56hjltgmtotVqoA2t_i_QY",
+    prefix: "!"
+}
---
+++ index.js
@@ -0,0 +1,46 @@
+const { Client, Collection } = require("discord.js");
+const client = new Client();
+require('discord-buttons')(client);
+const chalk = require("chalk");
+const fs = require("fs");
+const express = require('express')
+
+const app = express();
+
+const port = 3000
+
+app.get('/', (req, res) => res.send('Yo boi!!'))
+
+app.listen(port, () =>
+console.log(`Your app is listening a http://localhost:${port}`)
+);
+
+client.commands = new Collection();
+
+fs.readdir(__dirname + "/bot/events/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let event = require(__dirname + "/bot/events/" + file);
+        let eventName = file.split(".")[0];
+        console.log(
+            chalk.blue.bold("Loading event ") + chalk.magenta.bold(`"${eventName}"`)
+        );
+        client.on(eventName, event.bind(null, client));
+    });
+});
+
+fs.readdir(__dirname + "/bot/commands/", (err, files) => {
+    if (err) return console.error(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let props = require(__dirname + "/bot/commands/" + file);
+        let commandName = file.split(".")[0];
+        console.log(
+            chalk.blue.bold("Loading command ") + chalk.red.bold(`"${commandName}"`)
+        );
+        client.commands.set(commandName, props);
+    });
+});
+
+client.login(require("./config/bot").token).catch(err => console.log(chalk.red.bold(err)))
